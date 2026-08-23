24bdf7c10e09372177fff1a0fd1242dd851923ec
DELLIE\paulf <pfranke@inforit.nl>
2026-08-23 20:00:09 +0200

    cleanup

--- fix_nullx2f.py
+++
@@ -1,9 +0,0 @@
-with open('PacmanCS/Pacman.cs', 'r') as f:
-    text = f.read()
-
-text = text.replace('nullx2f', '0x2f')
-
-with open('PacmanCS/Pacman.cs', 'w') as f:
-    f.write(text)
-
-print("Fixed nullx2f typo!")
